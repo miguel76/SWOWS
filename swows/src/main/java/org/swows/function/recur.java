8e6e2b4c2c9eada8a9293253d286dfbc13d1e108
miguel <miguel.ceriani@gmail.com>
2013-01-30 19:45:58 +0100

    Defining recur function

---
+++ swows/src/main/java/org/swows/function/recur.java
@@ -0,0 +1,35 @@
+package org.swows.function;
+
+import java.util.List;
+
+import com.hp.hpl.jena.graph.Graph;
+import com.hp.hpl.jena.sparql.expr.NodeValue;
+import com.hp.hpl.jena.sparql.function.FunctionEnv;
+
+public class recur extends GraphReturningFunction {
+
+	@Override
+	public int getMinArgNum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaxArgNum() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Graph exec(List<NodeValue> args) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Graph exec(List<NodeValue> args, FunctionEnv env) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
